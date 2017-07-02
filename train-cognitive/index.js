65196f5f024ab643a5ff81a7d701030bfd68c0b7
kheiakiyama <kheiakiyama@gmail.com>
2017-07-02 14:08:19 +0900

    impl simple slack menu

---
+++ train-cognitive/index.js
@@ -0,0 +1,57 @@
+// You must include a context, but other arguments are optional
+module.exports = (context, data) => {
+  context.log('This function called');
+  context.res = {
+    "text": "Would you like to play a game?",
+    "response_type": "in_channel",
+    "attachments": [
+        {
+            "text": "Choose a game to play",
+            "fallback": "If you could read this message, you'd be choosing something fun to do right now.",
+            "color": "#3AA3E3",
+            "attachment_type": "default",
+            "callback_id": "game_selection",
+            "actions": [
+                {
+                  "name": "games_list",
+                  "text": "Pick a game...",
+                  "type": "select",
+                  "option_groups": [
+                    {
+                        "text": "Doggone bot antics",
+                        "options": [
+                                {
+                                    "text": "Unexpected sentience",
+                                    "value": "AI-2323"
+                                },
+                                {
+                                    "text": "Bot biased toward other bots",
+                                    "value": "SUPPORT-42"
+                                },
+                                {
+                                    "text": "Bot broke my toaster",
+                                    "value": "IOT-75"
+                                }
+                        ]
+                    },
+                    {
+                        "text": "Human error",
+                        "options": [
+                            {
+                                "text": "Not Penny's boat",
+                                "value": "LOST-7172"
+                            },
+                            {
+                                "text": "We built our own CMS",
+                                "value": "OOPS-1"
+                            }
+                        ]
+                    }
+                  ]
+                }
+            ]
+        }
+    ]
+  }
+  context.done();
+};
