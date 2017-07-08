6555804413f0feb1500bade54dcac834ba97139e
kheiakiyama <kheiakiyama@gmail.com>
2017-07-08 21:23:12 +0900

    fix when slack payload include image_url

--- slack-parser.js
+++ slack-parser.js
@@ -7,7 +7,21 @@ class SlackParser {
 
   parse() {
     const body = decodeURIComponent(this.body);
-    return querystring.parse(body);
+    const replace = this._replaceImageUrl(body);
+    return this._restoreImageUrl(querystring.parse(replace));
+  }
+
+  _restoreImageUrl(query) {
+    const re = /"dmy":""/;
+    const matches = re.exec(query.payload);
+    return { payload: query.payload.replace(re, this.imageUrl)};
+  }
+
+  _replaceImageUrl(body) {
+    const re = /"image_url":"(https)[^,]*"/;
+    const matches = re.exec(body);
+    this.imageUrl = matches[0];
+    return body.replace(re, '"dmy":""');
   }
 }
 
