a8f53316f9581261bee0c41dadc9bd353b499691
kheiakiyama <kheiakiyama@gmail.com>
2017-07-09 00:29:45 +0900

    fix parameter name

--- train-tag-queue/index.js
+++ train-tag-queue/index.js
@@ -4,7 +4,7 @@ const azure = require('azure-storage');
 // You must include a context, but other arguments are optional
 module.exports = (context) => {
     context.log('train-tag-queue called');
-    context.log(context.bindings.item);
+    context.log(context.bindings.myQueueItem);
     const queue = new TrainQueue(azure, process.env.KINTAI_STORAGE_CONTAINER);
-    queue.setTag(context.bindings.item);
+    queue.setTag(context.bindings.myQueueItem);
 };
